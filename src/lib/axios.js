3c073d0f44525893c79d22c9cb12e82473c21f36
ErebuzDark <106947202+ErebuzDark@users.noreply.github.com>
2025-07-31 10:56:45 +0800

    Fixed the CORS error sana

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL
+    ? "/api" // Vite proxy in dev
+    : "https://gomanga-api.vercel.app/api", // direct URL in production
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
